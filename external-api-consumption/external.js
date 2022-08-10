29c8824a0874018811362e74bd21689bdb088664
Lando Vargas <lando_vargas@outlook.com>
2022-08-10 02:46:08 -0400

    Added logic to call multiple promises for character endpoint So they would fulfill faster

--- external-api-consumption/external.js
+++ external-api-consumption/external.js
@@ -28,8 +28,34 @@ async function fetchJSON(url = null)
     return json.results.concat(await fetchJSON(json.info.next));;
 }
 
+async function fetchCharacters()
+{
+    let response = await fetch(knownURLS.characters)
+    let firstPage = await response.json();
+    
+    const promises = [];
+    for(let i = 2; i <= firstPage.info.pages; i++)
+    {
+        const url = `${knownURLS.characters}?page=${i}`;
+        promises.push(fetch(url));
+    }
+
+    const remainerPages = await Promise.allSettled(promises);
+    let results = firstPage.results;
+
+    for(let result of remainerPages)
+    {
+       const r = await result.value;
+       const nThPage = await r.json();
+       results = results.concat(nThPage.results);
+    }
+
+    return results;
+}
+
 module.exports = 
 {
     knownURLS,
-    fetchJSON
+    fetchJSON,
+    fetchCharacters
 }
