e4066b48be6507044b76e1d41c10adabc2035ce4
Lando Vargas <lando_vargas@outlook.com>
2022-08-10 00:24:50 -0400

    Refactored stats.js

--- nodejs/stats.js
+++ nodejs/stats.js
@@ -1,48 +1,49 @@
-const {knownURLS, fetchJSON} = require("./external-api-consumption/external.js");
-const {Characters} = require("./rules-object/characters");
-const {Episodes} = require("./rules-object/episodes");
-const {Locations} = require("./rules-object/locations");
-const {CharCounter} = require("./rules-object/charCounter");
-
-
 function saveJSON(name,json)
 {
     const fs = require("fs");
     fs.writeFileSync(`${name}.json`, json)
 }
 
-async function main()
+async function downloadData()
 {
-    const { performance } = require('perf_hooks');
-    const startTime = performance.now();
+    const {knownURLS, fetchJSON} = require("./external-api-consumption/external.js");
 
     const downloadStartTime = performance.now();
     console.log("Started downloading Episode data");
     const epStartTime = performance.now();
-    const e = await fetchJSON(knownURLS.episodes);
+    const episodesObj = await fetchJSON(knownURLS.episodes);
     const epEndTime = performance.now();
     console.log(`Finished downloading Episode data in ${(epEndTime - epStartTime) / 1000} seconds`);
 
     console.log("Started downloading Character data");
     const charStartTime = performance.now();
-    const crs = await fetchJSON(knownURLS.characters)
+    const charactersObj = await fetchJSON(knownURLS.characters);
     const charEndTime = performance.now();
     console.log(`Finished downloading Character data in ${(charEndTime - charStartTime) / 1000} seconds`);
 
     console.log("Started downloading Location data");
     const locationStartTime = performance.now();
-    const ls = await fetchJSON(knownURLS.location);
+    const locationObj = await fetchJSON(knownURLS.location);
     const locationEndTime = performance.now();
     console.log(`Finished downloading Location data in ${(locationEndTime - locationStartTime) / 1000} seconds`);
 
     const downloadEndTime = performance.now();
     console.log(`\n\n\n\nfinished downloading in ${(downloadEndTime - downloadStartTime) / 1000} seconds\n\n\n`);
 
+    return { episodesObj, charactersObj, locationObj };
+}
+
+function instantiateClasses(episodesObj, charactersObj, locationObj)
+{
+    const {Characters} = require("./rules-object/characters");
+    const {Episodes} = require("./rules-object/episodes");
+    const {Locations} = require("./rules-object/locations");
+
     const instantiationStartTime = performance.now();
     console.log("Started instantiating episodes");
     const epInstStartTime = performance.now();
     const episodes = new Episodes();
-    for(let eps of e)
+    for(let eps of episodesObj)
         episodes.addEpisode(eps);
     const epInstEndTime = performance.now();
     console.log(`Finished instantiating episodes in ${epInstEndTime - epInstStartTime} ms`);
@@ -50,7 +51,7 @@ async function main()
     console.log("Started instantiating characters");
     const charInstStartTime = performance.now();
     const characters = new Characters();
-    for(let char of crs)
+    for(let char of charactersObj)
         characters.addCharacter(char);
     const charInstEndTime = performance.now();
     console.log(`Finished instantiating characters in ${charInstEndTime - charInstStartTime} ms`);
@@ -58,7 +59,7 @@ async function main()
     console.log("Started instantiating locations");
     const locationInstStartTime = performance.now();
     const locations = new Locations();
-    for(let loc of ls)
+    for(let loc of locationObj)
         locations.addLocation(loc);
     const locationInstEndTime = performance.now();
     console.log(`Finished instantiating locations ${locationInstEndTime - locationInstStartTime} in ms`);
@@ -66,8 +67,19 @@ async function main()
     const instantiationEndTime = performance.now();
     console.log(`Finished instantiating everything in ${instantiationEndTime - instantiationStartTime} ms`)
 
-    
+    return {episodes, characters, locations}
+}
+
+async function main()
+{
+    const { performance } = require('perf_hooks');
+    const startTime = performance.now();
+
+    const {episodesObj, charactersObj, locationObj} = await downloadData();
+    const {episodes, characters, locations} = instantiateClasses(episodesObj, charactersObj, locationObj);
+
     console.log("Started processing...");
+    const {CharCounter} = require("./rules-object/charCounter");
     const charCounter = new CharCounter();
     saveJSON("chars",JSON.stringify(characters.characters, null, 4));
     saveJSON("locations",JSON.stringify(locations.locations, null, 4));
