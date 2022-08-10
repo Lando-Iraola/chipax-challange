1eebf588a38760403fb856b0b3043e1b05e56008
Lando Vargas <lando_vargas@outlook.com>
2022-08-10 01:44:39 -0400

    Refactoring EpisodeLocations

--- rules-object/episodeLocations.js
+++ rules-object/episodeLocations.js
@@ -12,30 +12,29 @@ class EpisodeLocations
         
         for(let ep in episodes.episodes)
         {
-            console.log("here:", ep);
             const episode = episodes.episodes[ep];
-            const requested_data = {};
-            requested_data["name"] = episode.name;
-            requested_data["episode"] = episode.episode;
             const hashLocations = {};
             for(let char of episode.characters)
             {
                 const character = characters.characters[char];
                 if(locs[character.origin])
-                {
                     hashLocations[locs[character.origin].name] = locs[character.origin].name;
-                }
                 else
-                {
                     hashLocations["unknown"] = "unknown";
-                }
+                
             }
 
-            requested_data["count"] = Object.keys(hashLocations).length;
-            requested_data["locations"] = Object.keys(hashLocations);
-            console.log(requested_data);
+            const requested_data = 
+            {
+                name : episode.name,
+                episode : episode.episode,
+                count : Object.keys(hashLocations).length,
+                locations : Object.keys(hashLocations)
+            }
+            
             results.push(requested_data);
         }
+        
         const structure = 
         {
             exercise_name: "Episode locations",
